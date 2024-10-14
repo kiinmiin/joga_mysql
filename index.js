fab319c5a3922707cd7a1ebbd915afa67bf8298f
kiinmiin <rasmo.kiin@voco.ee>
2024-10-14 21:20:55 +0300

    Displayed all articles

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const path = require('path')
 const hbs = require('express-handlebars');
 // Seadistab malli mootori kataloogi ja failide laiendid
 app.set('views', path.join(__dirname, 'views'));
-app.set('view-engine', 'hbs');
+app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
@@ -16,11 +16,12 @@ app.engine('hbs', hbs.engine({
 // Seadistab staatilise avaliku kataloogi
 app.use(express.static('public'));
 
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 
+// loob andmebaasiga ühenduse
 var con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -33,6 +34,21 @@ con.connect((err) => {
     console.log('Connected to joga_mysql db')
 })
 
+// näita kõiki artikkleid - index leht
+app.get('/', (req, res) =>{
+    let query = "SELECT * FROM article";
+    let articles = [] 
+    con.query(query, (err, result) => {
+        if (err) throw(err);
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })  
+});
+
+
+// rakenduse algus punkt
 app.listen(3003, () => {
     console.log('App is started at http://localhost:3003')
 })
