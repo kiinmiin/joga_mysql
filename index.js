b63a599d820da9d4fbc58377c09615511fd11b7f
kiinmiin <rasmo.kiin@voco.ee>
2024-10-16 16:34:48 +0300

    Created article and author controller and route, refactored application code

--- index.js
+++ index.js
@@ -16,23 +16,16 @@ app.engine('hbs', hbs.engine({
 // Seadistab staatilise avaliku kataloogi
 app.use(express.static('public'));
 
-const mysql = require('mysql2')
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
 
-// loob andmebaasiga ühenduse
-var con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'qwerty',
-    database: 'joga_mysql'
-})
+const articleRoutes = require('./routes/article'); // import article route
 
-con.connect((err) => {
-    if(err) throw err;
-    console.log('Connected to joga_mysql db')
-})
+// to use article routes
+app.use('/', articleRoutes);
+app.use('/article', articleRoutes);
+app.use('/author', articleRoutes)
 
 // näita kõiki artikkleid - index leht
 app.get('/', (req, res) =>{
